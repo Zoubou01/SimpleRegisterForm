ddf531473f6a66db48372eef388bf4a2efe36f5b
Giannis Zouboulakis <Zoubou01>
2022-07-01 22:13:40 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React , {useState} from 'react';
+
 import './App.css';
 
-function App() {
+function App(){
+  const [values,setValues] = useState ({
+    fistName:"",
+    lastName:"",
+    email:"",
+  });
+  const [fn,setFn] = useState(true);
+  const [ln,setLn] = useState(true);
+  const [em,setEm] = useState(true);
+  const [register,setRegister] = useState(false);
+
+
+  const handleFirstNameInputChange = (event) => {
+    setValues({...values, firstName: event.target.value})
+    setFn(false);
+  }
+  const handleLastNameInputChange = (event) => {
+    setValues({...values, lastName: event.target.value})
+    setLn(false);
+  }
+  const handleEmailInputChange = (event) => {
+    setValues({...values, email: event.target.value})
+    setEm(false);
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+      setRegister(true);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className='forma-container'>
+    <form className='forma' onSubmit={handleSubmit}>
+      <input 
+      value={values.firstName}
+      className='la'
+      placeholder='First Name'
+      name='firstName' 
+      onChange={handleFirstNameInputChange}/>
+      {fn && register ? <span>Please Enter First Name</span> : null }
+      <input 
+      value={values.lastName}
+      className='la'
+      placeholder='Last Name'
+      name='lastName' 
+      onChange={handleLastNameInputChange}/>
+      {ln && register ? <span>Please Enter Last Name</span> : null }
+        <input 
+      value={values.email}
+      className='la'
+      placeholder='Email'
+      name='email' 
+      onChange={handleEmailInputChange}/>
+      {em && register ? <span>Please Enter Email</span> : null }
+        <button classname='la'
+        type='submit'>
+          Register
+        </button>
+    </form>
+  </div>
+  )
 }
 
 export default App;
